c75eefd4c00a28f50decf0e30838248cedd53a07
kore4n <kwan.jason2001@gmail.com>
2022-02-22 03:17:21 -0500

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -13,12 +13,12 @@ module.exports = {
             const connection = joinVoiceChannel({
                 channelId: voice_channel.id,
                 guildId: voice_channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator,
+                adapterCreator: voice_channel.guild.voiceAdapterCreator,
             })
             message.channel.send("Kore4n Bot has joined the voice channel!")
         }
         else if (cmd === "l" || cmd === "leave"){
-            const connection = getVoiceConnection(message.guild.id)
+            const connection = getVoiceConnection(voice_channel.guild.id)
             if(!connection) return message.channel.send("I'm not in a voice channel!")
             connection.destroy()
             message.channel.send("Kore4n Bot has left the voice channel!")
