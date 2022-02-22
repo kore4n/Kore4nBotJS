e3f5147506beff694425954377e22ef656434024
kore4n <kwan.jason2001@gmail.com>
2022-02-22 02:43:09 -0500

    update join.js

--- commands/join.js
+++ commands/join.js
@@ -4,7 +4,11 @@ module.exports = {
     name: 'join',
     aliases: ['j', 'l', 'leave'],
     description: "sends the youtube link!",
-    execute(message, args, cmd){
+    execute(message, cmd){
+        const voice_channel = message.member.voice.channel;
+        if (!voice_channel) return message.channel.send('You need to be in a voice channel to execute this command!');
+        if (!voice_channel.joinable) return message.channel.send('I need permission to join your voice channel!')
+
         if (cmd == 'join' || cmd == "j"){
             const connection = joinVoiceChannel({
                 channelId: voice_channel.id,
@@ -14,6 +18,8 @@ module.exports = {
         }
         else if (cmd == "l" || cmd == "leave"){
             const connection = getVoiceConnection(message.guild.id)
+            if(!connection) return message.channel.send("I'm not in a voice channel!")
+            connection.destroy()
             message.channel.send("Kore4n Bot has left the voice channel!")
         }
     }
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs')
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"]});
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_VOICE_STATES"]});
 
 
 client.commands = new Discord.Collection();
