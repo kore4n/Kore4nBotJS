7c64b40304c41a9926d3a529cee84adb3d6bffdd
kore4n <kwan.jason2001@gmail.com>
2022-02-22 02:58:06 -0500

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -9,14 +9,15 @@ module.exports = {
         if (!voice_channel) return message.channel.send('You need to be in a voice channel to execute this command!');
         if (!voice_channel.joinable) return message.channel.send('I need permission to join your voice channel!')
 
-        if (cmd == 'join' || cmd == "j"){
+        if (cmd === 'join' || cmd === "j"){
             const connection = joinVoiceChannel({
                 channelId: voice_channel.id,
-                guildId: voice_channel.guild.id
+                guildId: voice_channel.guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator,
             })
             message.channel.send("Kore4n Bot has joined the voice channel!")
         }
-        else if (cmd == "l" || cmd == "leave"){
+        else if (cmd === "l" || cmd === "leave"){
             const connection = getVoiceConnection(message.guild.id)
             if(!connection) return message.channel.send("I'm not in a voice channel!")
             connection.destroy()
@@ -24,5 +25,6 @@ module.exports = {
         }
 
         message.channel.send("Command registered")
+        message.channel.send(cmd)
     }
 }
